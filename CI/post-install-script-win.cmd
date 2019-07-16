dbd7af3d6701d1d83ecaa92bb65d3f201e8f1754
Rat <52238970+ratwithacompiler@users.noreply.github.com>
2019-07-16 08:27:36 +0200

    CI: add release artifact only containing 32bit/64bit folders with the respective .dll

--- CI/post-install-script-win.cmd
+++ CI/post-install-script-win.cmd
@@ -1,2 +1,11 @@
 
 dir
+mkdir release_dir
+mkdir release_dir\32bit
+mkdir release_dir\64bit
+
+copy build_32\RelWithDebInfo\obs_google_caption_plugin.dll release_dir\32bit\obs_google_caption_plugin.dll
+copy build_64\RelWithDebInfo\obs_google_caption_plugin.dll release_dir\64bit\obs_google_caption_plugin.dll
+
+dir release_dir\32bit
+dir release_dir\64bit
