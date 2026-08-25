109e028521c79e8e2068b4d2713a449c042493ff
Rat <52238970+ratwithacompiler@users.noreply.github.com>
2026-08-25 08:28:54 +0200

    fix windows GRPC builds

--- lib/caption_stream/speech_apis/grpc_speech_api/CaptionStream.h
+++ lib/caption_stream/speech_apis/grpc_speech_api/CaptionStream.h
@@ -31,6 +31,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "ThreadsaferCallback.h"
 #include "CaptionResult.h"
+#include <sstream>
 
 typedef unsigned int uint;
 using namespace std;
